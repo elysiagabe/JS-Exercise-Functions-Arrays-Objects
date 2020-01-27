fa71ee11326a9845cd1fef3678a894e95cf43b52
Elysia Gabe <elysiagabe@gmail.com>
2020-01-26 20:06:41 -0800

    Answers Challenge 'makeSmartPerson'

--- index.js
+++ index.js
@@ -77,8 +77,17 @@ function getName(person) {
  *         and returns a string like `Hello, my name is {name}`.
  *         where `{name}` is the name passed into `makeSmartPerson`.
 */
-function makeSmartPerson(/* code here */) {
-  /* code here */
+function makeSmartPerson(name) {
+  var object = {
+    name: name,
+    sum: function(num1, num2){
+      return num1 + num2;
+    },
+    speak: function(){
+      return "Hello, my name is " + name;
+    }
+  };
+  return object;
 }
 
 
